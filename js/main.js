829ee847d6e16f395bd14105bb30aab7e5cc0662
JurayevJovohir <javokhake1@gmail.com>
2022-12-06 17:29:45 -0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,67 @@
+// EXAM 1 :
+// function addition(a, b) {
+// 	return a + b;
+// }
+
+// --------------------------------------
+
+// EXAM 2 :
+// function convert(minutes) {
+// 	return minutes*60;
+// }
+
+// --------------------------------------
+
+// EXAM 3 :
+// function addition(num) {
+// 	return num + 1;
+// }
+
+// --------------------------------------
+
+// EXAM 4 :
+// function triArea(base, height) {
+// 	return base * height /2
+// }
+
+// --------------------------------------
+
+// EXAM 5 :
+// function calcAge(age) {
+// 	return age*365;
+// }
+
+// --------------------------------------
+
+// EXAM 6 :
+// function remainder(x, y) {
+// 	return x % y;
+// }
+
+// --------------------------------------
+
+// EXAM 7 :
+// function footballPoints(wins, draws, losses) {
+// 	return wins*3 + draws*1;
+// }
+
+// --------------------------------------
+
+// EXAM 8 :
+// function points(twoPointers, threePointers) {
+// 	return twoPointers*2 + threePointers*3;
+// }
+
+// --------------------------------------
+
+// EXAM 9 :
+// function lessThan100(a, b) {
+// 	return a+b<100;
+// }
+
+// --------------------------------------
+
+// EXAM 10 :
+// function animals(chickens, cows, pigs) {
+// 	return chickens*2 + (cows + pigs)*4;
+// }
